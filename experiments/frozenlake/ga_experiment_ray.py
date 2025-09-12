e1708afc26f44366860b3d85cce1c4f205173a86
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-09-12 20:55:31 +0100

    Added new objective group `auc_kl_source_target` in `ga_experiment_ray.py` to enhance objective grouping flexibility.

--- experiments/frozenlake/ga_experiment_ray.py
+++ experiments/frozenlake/ga_experiment_ray.py
@@ -50,6 +50,7 @@ SOURCE_FACTORY_PATH = "experiment_utils.env_factories:make_frozenlake"
 OBJECTIVE_GROUPS: Dict[str, List[str]] = {
     "auc_source_target": ["auc_p1_source", "auc_p2"],
     "perf_source_target": ["int_rand_to_source_on_source", "int_source_to_target"],
+    "auc_kl_source_target": ["auc_p1_source", "minus_target_kl"],
     "perf_kl_source_target": ["int_rand_to_source_on_source", "minus_target_kl"],
     "kl": ["minus_control_kl", "minus_target_kl"],
     "auc_source_value_diff": ["auc_p1_source", "minus_value_diff"],
